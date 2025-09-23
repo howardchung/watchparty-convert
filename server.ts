a5716fd569c13269e8e3c5045c41c2abf9bdc81e
Howard Chung <howardzchung@gmail.com>
2025-09-23 07:57:38 +0000

    add additional output configs

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 import 'dotenv/config';
-import Fastify, { type FastifyReply } from 'fastify';
-import cors from '@fastify/cors';
 import cp from 'node:child_process';
 import fs from 'node:fs';
+import { WebSocketServer, type WebSocket } from 'ws';
+import http from 'node:http';
+import https from 'node:https';
+import express from 'express';
 
 // http server (or express?)
 // client generates uuid on upload to /upload/:id endpoint
@@ -29,72 +31,137 @@ const key = process.env.SSL_KEY_FILE
 const cert = process.env.SSL_CRT_FILE
   ? fs.readFileSync(process.env.SSL_CRT_FILE).toString()
   : '';
-const rooms = new Map<string, Map<string, FastifyReply>>();
-const fastify = Fastify({ http2: true, https: { key, cert }, logger: true });
-await fastify.register(cors, {
-  origin: '*',
-});
+const rooms = new Map<string, Map<string, WebSocket>>();
+const port = Number(process.env.PORT) || 80;
+const app = express();
+const server = (key && cert) ? https.createServer({ key, cert }, app) : http.createServer(app);
+const wss = new WebSocketServer({ server });
+server.listen(port);
 
-fastify.post('/:id', (req, res) => {
-  const id = (req.params as any).id;
-  const room = new Map<string, FastifyReply>();
-  rooms.set(id, room);
-  console.log('upload', id);
-  let ffmpeg: cp.ChildProcessWithoutNullStreams | null = cp.spawn('ffmpeg', [
-    '-re',
-    '-i',
-    'pipe:',
-    '-c:v',
-    'libx264',
-    '-preset',
-    'veryfast',
-    '-c:a',
-    'aac',
-    '-ac',
-    '2',
-    '-f',
-    'mpegts',
-    '-',
-  ]);
-  ffmpeg.stdout.on('data', (data) => {
-    // Write chunk to any connected consumers by consulting map/id
-    for (let res of room.values()) {
-      res.raw.write(data);
-    }
-  });
-  ffmpeg.stderr.on('data', (data) => {
-    console.log(data.toString());
-  });
-  const handleClose = () => {
-    console.log('upload close');
-    ffmpeg?.kill();
-    ffmpeg = null;
-    rooms.delete(id);
-    res.send();
-  };
-  req.raw.pipe(ffmpeg.stdin).once('error', handleClose);
-  req.raw.once('close', handleClose);
-});
-fastify.get('/:id', (req, res) => {
-  const id = (req.params as any).id.replace('.ts', '');
-  console.log('download', id);
-  const room = rooms.get(id);
-  if (!room) {
-    return res.status(404).send();
+app.get('/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const { range } = req.headers;
+  let streamOptions = undefined;
+  let { size: fileSize } = fs.statSync('/tmp/' + id);
+  if (range) {
+    const [s, e] = range.replace("bytes=", "").split("-");
+    const start = Number(s);
+    const end = Number(e) || fileSize - 1;
+    streamOptions = { start, end };
+    res.append("Accept-Ranges", "bytes");
+    res.append("Content-Range", `bytes ${start}-${end}/${fileSize}`);
+    res.append("Content-Length", String(end - start + 1));
+    res.status(206);
+  } else {
+    res.append("Content-Length", String(fileSize));
   }
-  res.raw.setHeader('access-control-allow-origin', '*');
-  const rand = String(Math.random());
-  // Add res to the map of connected clients for this id
-  //@ts-expect-error
-  room.set(rand, res);
-  console.log(room);
-  // On end/close, remove res from map
-  req.raw.once('close', () => {
-    room.delete(rand);
-    res.send();
-  });
+  res.append('Access-Control-Allow-Origin', '*');
+  res.append("Content-Type", "video/mp4");
+  const fileStream = fs.createReadStream('/tmp/' + id, streamOptions);
+  fileStream.pipe(res);
 });
 
-const port = Number(process.env.PORT) ?? 80;
-await fastify.listen({ port, host: '0.0.0.0' });
-// console.log('listening', port);
+wss.on('connection', (ws, req) => {
+  console.log(req.url);
+  const id = req.url?.split('/').slice(-1)[0];
+  if (!id) {
+    console.log('no id');
+    return;
+  }
+  if (!rooms.has(id)) {
+    // Publisher
+    const room = new Map<string, WebSocket>();
+    rooms.set(id, room);
+    // ws.send('wss://azure.howardchung.net:5001/' + id);
+    // let ffmpeg: cp.ChildProcessWithoutNullStreams | null = cp.spawn('ffmpeg', [
+    //   '-re',
+    //   '-i',
+    //   'pipe:',
+    //   '-c:v',
+    //   'libx264',
+    //   '-preset',
+    //   'veryfast',
+    //   '-c:a',
+    //   'aac',
+    //   '-ac',
+    //   '2',
+    //   '-f',
+    //   'mpegts',
+    //   '-',
+    // ]);
+    // let ffmpeg: cp.ChildProcessWithoutNullStreams | null = cp.spawn('ffmpeg', [
+    //   '-i',
+    //   'pipe:',
+    //   '-c:v',
+    //   'libx264',
+    //   '-preset',
+    //   'veryfast',
+    //   '-c:a',
+    //   'aac',
+    //   '-ac',
+    //   '2',
+    //   '-f',
+    //   'mp4',
+    //   '-movflags',
+    //   'frag_keyframe+empty_moov+faststart',
+    //   '/tmp/' + id,
+    // ]);
+    let ffmpeg: cp.ChildProcessWithoutNullStreams | null = cp.spawn('ffmpeg', [
+      '-re',
+      '-i',
+      'pipe:',
+      '-c:v',
+      'libx264',
+      '-preset',
+      'veryfast',
+      '-c:a',
+      'aac',
+      '-ac',
+      '2',
+      '-f',
+      'hls',
+      '/tmp/' + id + '.m3u8',
+    ]);
+    let urlSent = false;
+    ws.on('message', (data) => {
+      const sendNext = ffmpeg?.stdin.write(data);
+      if (sendNext) {
+        ws.send('1');
+      } else {
+        ffmpeg?.stdin.once('drain', () => {
+          ws.send('1');
+        });
+      }
+    });
+    ffmpeg.stdout.on('data', (data) => {
+      // Write chunk to any connected consumers by consulting map/id
+      for (let res of room.values()) {
+        res.send(data);
+      }
+    });
+    ffmpeg.stderr.on('data', (data) => {
+      console.log(data.toString());
+      if (data.toString().includes(`${id}.m3u8.tmp`) && !urlSent) {
+        ws.send('https://azure.howardchung.net:5001/' + id + '.m3u8');
+        urlSent = true;
+      }
+    });
+    const handleClose = () => {
+      console.log('upload close');
+      ffmpeg?.kill();
+      ffmpeg = null;
+      rooms.delete(id);
+    };
+    ws.once('close', handleClose);
+    ws.once('error', handleClose);
+  } else {
+    // Subscriber
+    const room = rooms.get(id);
+    const rand = String(Math.random());
+    room?.set(rand, ws);
+    ws.once('close', () => {
+      room?.delete(rand);
+    });
+  }
+});
